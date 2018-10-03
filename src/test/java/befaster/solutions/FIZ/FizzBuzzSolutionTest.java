58d106f99b2cf617d635a8f8155f66b059625c95
sbx_user1060 <sbx_user1060@ip-10-129-149-151.eu-west-2.compute.internal>
2018-10-03 15:35:10 +0000

    Wed Oct 03 15:35:10 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,4 +1,4 @@
-package befaster.solutions.SUM;
+package befaster.solutions.FIZ;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -6,22 +6,32 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class SumSolutionTest {
-    private SumSolution sum;
+public class FizzBuzzSolutionTest {
+    private FizzBuzzSolution fiz;
 
     @Before
     public void setUp() {
 
-        sum = new SumSolution();
+        fiz = new FizzBuzzSolution();
     }
 
     @Test
-    public void compute_sum() {
-        assertThat(sum.compute(1, 1), equalTo(2));
+    public void compute_fiz() {
+        assertThat(fiz.fizzBuzz(1), equalTo("1"));
     }
 
     @Test
-    public void compute_sum2() {
-        assertThat(sum.compute(100, 100), equalTo(200));
+    public void compute_fiz3() {
+        assertThat(fiz.fizzBuzz(3), equalTo("fizz"));
+    }
+
+    @Test
+    public void compute_fiz5() {
+        assertThat(fiz.fizzBuzz(5), equalTo("buzz"));
+    }
+
+    @Test
+    public void compute_fiz15() {
+        assertThat(fiz.fizzBuzz(15), equalTo("fizz buzz"));
     }
 }
