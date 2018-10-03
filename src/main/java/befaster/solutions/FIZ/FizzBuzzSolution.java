012f52b5e00f4b2eee39eff95adce0c1c985198b
sbx_user1060 <sbx_user1060@ip-10-129-149-151.eu-west-2.compute.internal>
2018-10-03 15:29:11 +0000

    Wed Oct 03 15:29:11 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,7 +5,19 @@ import befaster.runner.SolutionNotImplementedException;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+        StringBuilder sb = new StringBuilder();
+        if (number % 3 == 0) {
+            sb.append("fizz");
+        }
+        if (number % 15 == 0) {
+            sb.append(" ");
+        }
+
+        if (number % 5 == 0) {
+            sb.append("buzz");
+        }
+        return sb.toString();
+
     }
 
 }
